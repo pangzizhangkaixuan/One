70f647387c0751296d8cab089b3c00f5a323aebf
pangzizhangkaixuan <1309482963@qq.com>
2019-03-12 15:15:54 +0800

    练习一

---
+++ zkx/src/main/java/effectiveJava/one/Demo.java
@@ -0,0 +1,10 @@
+package effectiveJava.one;
+
+public class Demo {
+	public static void main(String[] args) {
+		NutritionFacts cocaCola = new NutritionFacts.Builder(240, 8)
+				.calories(100).sodium(35).carbohydrate(27).build();
+		
+		System.out.println(cocaCola.toString());
+	}
+}
